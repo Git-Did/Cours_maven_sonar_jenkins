b520e8fc82fc955025fd79179f4b77ca8e1e2d00
Cheng Jun <d.cheng@live.fr>
2020-05-06 15:40:51 +0200

    Start

---
+++ XVI - DevOps/04_demo_javase_maven_sonar/src/main/java/com/intiformation/maven_sonar/Calculatrice.java
@@ -0,0 +1,14 @@
+package com.intiformation.maven_sonar;
+
+public class Calculatrice {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int mul(int a, int b) {
+		return a * b;
+	}
+	
+	
+}//end class
